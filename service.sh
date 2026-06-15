dba100aef2b2807f4fd4b0c123a955d4a6fd2b58
rev.s <wassermann.fabi@gmail.com>
2026-06-15 15:24:59 +0200

    Refactor volume key event handling in service.sh
    Refactor volume key event handling using awk for improved readability and performance.
    display on first check makes stuff more reliable. less performance impact when display on

--- service.sh
+++ service.sh
@@ -1,34 +1,33 @@
 #!/system/bin/sh
 CONFIG_FILE="/data/adb/.config/volume_skip/ig.conf"
-
+if [ -z ${LOADED+x} ]; then
 if [ -f "$CONFIG_FILE" ]; then
-  . "$CONFIG_FILE"
+    . "$CONFIG_FILE"
+    LOADED="1"
 else
   exit 1
 fi
-
-is_display_on() {
-  [ -n "$DISPLAY_VAR" ] && dumpsys power | grep "$DISPLAY_VAR" | grep -q "$DISPLAY_EXPECT"
-}
-
+fi
 start_ts=""
 
-getevent -t -l "$VOLUME_DEVICE" | while read -r line; do
-  if echo "$line" | grep -q "KEY_VOLUMEUP.*DOWN"; then
-    start_ts=$(date +%s%3N)
-  fi
-
-  if echo "$line" | grep -q "KEY_VOLUMEUP.*UP"; then
-    if [ -n "$start_ts" ]; then
-      end_ts=$(date +%s%3N)
-      delta=$((end_ts - start_ts))
-      if [ "$delta" -ge "$LONG_PRESS_MS" ]; then
-        if ! is_display_on; then
-          input keyevent 25
-          input keyevent 87
-        fi
-      fi
-    fi
-    start_ts=""
-  fi
-done &
+getevent -t -l "$VOLUME_DEVICE" | awk -v long_press="$LONG_PRESS_MS" -v dvar="$DISPLAY_VAR" -v dexp="$DISPLAY_EXPECT" '
+/KEY_VOLUMEUP.*DOWN/ {
+    display_on = (system("dumpsys power | grep \"" dvar "\" | grep -q \"" dexp "\"") == 0)
+    if (!display_on) {
+        "date +%s%3N" | getline start_ts
+        close("date +%s%3N")
+    }
+  }
+  /KEY_VOLUMEUP.*UP/ {
+    if (start_ts != "") {
+        "date +%s%3N" | getline end_ts
+        close("date +%s%3N")
+        delta = end_ts - start_ts
+        if (delta >= long_press) {
+          system("input keyevent 25")
+          system("input keyevent 87")
+        }
+    }
+    start_ts = ""
+  }
+'
